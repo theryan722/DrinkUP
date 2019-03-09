323d33119fb82b9869e1a4cdb6b5a6cefff29bb4
Ryan O'Day <romanguy722@gmail.com>
2019-03-09 10:44:23 -0500

    Get rid of debug console messages

--- src/scripts/routerhelper.ts
+++ src/scripts/routerhelper.ts
@@ -1,10 +1,8 @@
 //Checks if user is logged in before loading route
 function routerCheckUserAuthentication(routeTo: any, routeFrom: any, resolve: any, reject: any): any {
     if (userIsLoggedIn()) {
-        console.log('aaa')
         resolve();
     } else {
-        console.log('bbb')
         reject();
         loadSignInPage();
     }
