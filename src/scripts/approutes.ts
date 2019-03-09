97b5920fe4f2ce0bd297cb2c63f5886d134f1dba
Ryan O'Day <romanguy722@gmail.com>
2019-03-09 11:47:54 -0500

    Setup signin yeti

--- src/scripts/approutes.ts
+++ src/scripts/approutes.ts
@@ -9,7 +9,6 @@ var _APPROUTES = [
         name: 'signin',
         path: '/signin',
         async(routeTo: any, routeFrom: any, resolve: any, reject: any) {
-            console.log('wtf')
             if (userIsLoggedIn()) {
                 reject();
                 mainView.router.navigate('/');
---
+++ src/scripts/pageinit.ts
@@ -0,0 +1,12 @@
+//Wrapper function for page init event to prevent event from firing when going back
+function onPageInit(pagename: string, callback: any, initialonly = true) {
+    $$(document).on('page:init', '.page[data-name="' + pagename + '"]', function (e: any) {
+        if (initialonly) {
+            if (e.detail.direction !== 'backward') {
+                callback(e);
+            }
+        } else {
+            callback(e);
+        }
+    });
+}
